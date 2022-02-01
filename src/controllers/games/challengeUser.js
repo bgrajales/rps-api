8b6869e1489073e1a926b102385087fd11cce592
bgrajales <82546939+bgrajales@users.noreply.github.com>
2022-02-01 14:25:52 -0300

    v 0.9.2
    - update to challengeUser

--- src/controllers/games/challengeUser.js
+++ src/controllers/games/challengeUser.js
@@ -22,7 +22,6 @@ module.exports = async( request, response ) => {
             return game.player2.userName === challengedName;
         });
 
-        console.log(alreadyChallenged);
 
         if(alreadyChallenged !== undefined) {
             // response.status(400).send('You have already challenged this user!');
@@ -31,57 +30,75 @@ module.exports = async( request, response ) => {
             });
         } else {
 
-            const id = mongoose.Types.ObjectId();
-
-            const newUserGame = {
-                _id: id,
-                player1: {
-                    id: userId,
-                    userName: userName
-                },
-                player2: {
-                    id: challenged,
-                    userName: challengedName
-                },
-            }
-
-            user.activeGames.unshift(newUserGame);
-            user.save();
-
             userModel.findOne({
                 _id: challenged
             }).then( challengedUser => {
 
-                const newChallengedGame = {
+                const alreadyChallenged = challengedUser.activeGames.find( game => {
+                    return game.player2.userName === userName;
+                });
+
+                if(alreadyChallenged !== undefined) {
+                    response.status(400).send('You have already been challenged by this user!');
+                } else {
+
+
+                const id = mongoose.Types.ObjectId();
+
+                const newUserGame = {
                     _id: id,
                     player1: {
-                        id: challenged,
-                        userName: challengedName
-                    },
-                    player2: {
                         id: userId,
                         userName: userName
                     },
+                    player2: {
+                        id: challenged,
+                        userName: challengedName
+                    },
                 }
 
-                const newNotification = {
-                    id: mongoose.Types.ObjectId(),
-                    message: `${userName} has challenged you!`,
-                    challenger: userName,
-                    gameId: id,
-                }
+                user.activeGames.unshift(newUserGame);
+                user.save();
 
-                challengedUser.notifications.unshift(newNotification);
-                challengedUser.activeGames.unshift(newChallengedGame);
-                challengedUser.save();
+                userModel.findOne({
+                    _id: challenged
+                }).then( challengedUser => {
 
-                response.status(200).send({
-                    message: 'Challenge sent!',
-                    gameId: id
+                    const newChallengedGame = {
+                        _id: id,
+                        player1: {
+                            id: challenged,
+                            userName: challengedName
+                        },
+                        player2: {
+                            id: userId,
+                            userName: userName
+                        },
+                    }
+
+                    const newNotification = {
+                        id: mongoose.Types.ObjectId(),
+                        message: `${userName} has challenged you!`,
+                        challenger: userName,
+                        gameId: id,
+                    }
+
+                    challengedUser.notifications.unshift(newNotification);
+                    challengedUser.activeGames.unshift(newChallengedGame);
+                    challengedUser.save();
+
+                    response.status(200).send({
+                        message: 'Challenge sent!',
+                        gameId: id
+                    });
+                }).catch( error => {
+                    console.log(error);
                 });
+                }
             }).catch( error => {
                 console.log(error);
             });
+
         }
     }).catch( error => {
         console.log(error);
